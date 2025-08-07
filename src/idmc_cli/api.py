071f5b73f20fa65169ea7d483db55b88384d9565
Jonathon Bowring <jbowring@informatica.com>
2025-08-07 20:34:23 +1000

    Added support to delete roles

--- src/idmc_cli/api.py
+++ src/idmc_cli/api.py
@@ -846,7 +846,7 @@ class InformaticaCloudAPI:
         resp = ''
         attempts = 0
         
-        # Lookup the user group id if needed
+        # Lookup the role id if needed
         if name:
             lookup = self.getRoles(name=name, debug=debug)
             try:
@@ -913,6 +913,65 @@ class InformaticaCloudAPI:
                 break
             else:
                 resp = r.json()
+                break
+        
+        return resp
+    
+
+    def deleteRole(self, id=None, name=None, debug=False):
+        """This function deletes an IDMC role"""
+        
+        # Check if cli has been configured
+        if not self.username:
+            return 'CLI needs to be configured. Run the command "idmc configure"'
+        
+        # Lookup the role id if needed
+        if name:
+            lookup = self.getRoles(name=name, debug=debug)
+            try:
+                id = lookup[0]['id']
+            except Exception as e:
+                return {
+                        'status': 500,
+                        'text': f'Unable to find role id for { name }'
+                    }
+        
+        attempts = 0
+        
+        while True:
+        
+            # Execute the API call
+            url = f'https://{ self.pod }.{ self.region }.informaticacloud.com/saas/public/core/v3/roles/{ quote(id) }'
+            headers = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'INFA-SESSION-ID': self.session_id }
+            r = requests.delete(url, headers=headers)
+
+            if debug:
+                self.debugRequest(r, attempts)
+
+            # Check for expired session token
+            if r.status_code == 401 and attempts <= self.max_attempts:
+                self.login()
+                attempts = attempts + 1
+                continue
+            # Abort after the maximum number of attempts
+            elif attempts > self.max_attempts:
+                resp = {
+                    'status': r.status_code,
+                    'text': r.text
+                }
+                break
+            # Else if there is an unexpected error return a failure
+            elif r.status_code < 200 or r.status_code > 299:
+                resp = {
+                    'status': r.status_code,
+                    'text': r.text
+                }
+                break
+            elif r.status_code == 204:
+                resp = { 'message': 'Role deleted' }
+                break
+            # Break when there are no pages left
+            else:
                 break
         
         return resp
