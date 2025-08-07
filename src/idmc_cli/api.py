11aa117d15694de65e7af6a561de97f1a3df765f
jonbowring <jbowring@informatica.com>
2025-08-07 20:26:31 +1000

    Added support for removing privileges (#20)

--- src/idmc_cli/api.py
+++ src/idmc_cli/api.py
@@ -836,6 +836,88 @@ class InformaticaCloudAPI:
         return resp
     
 
+    def removeRolePrivileges(self, id, name, privilegeIds, privilegeNames, debug=False):
+        """This function removes privileges from a role"""
+        
+        # Check if cli has been configured
+        if not self.username:
+            return 'CLI needs to be configured. Run the command "idmc configure"'
+        
+        resp = ''
+        attempts = 0
+        
+        # Lookup the user group id if needed
+        if name:
+            lookup = self.getRoles(name=name, debug=debug)
+            try:
+                id = lookup[0]['id']
+            except Exception as e:
+                return {
+                        'status': 500,
+                        'text': f'Unable to find role id for { name }'
+                    }
+        
+        # Lookup the privilege ids if needed
+        if privilegeIds:
+            privilegeNames = []
+            privileges = privilegeIds.split(',')
+            for privilege in privileges:
+                lookup = self.getPrivileges(id=privilege, debug=debug)
+                try:
+                    privilegeName = lookup[0]['name']
+                    privilegeNames.append(privilegeName)
+                except Exception as e:
+                    return {
+                        'status': 500,
+                        'text': f'Unable to find privilege id { privilege }'
+                    }
+        elif privilegeNames:
+            privilegeNames = privilegeNames.split(',')
+
+        while True:
+        
+            # Prepare the mandatory fields
+            data = {
+                'privileges': privilegeNames
+            }
+            
+            # Execute the API call
+            url = f'https://{ self.pod }.{ self.region }.informaticacloud.com/saas/public/core/v3/roles/{ quote( id ) }/removePrivileges'
+            headers = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'INFA-SESSION-ID': self.session_id }
+            r = requests.put(url, headers=headers, json=data)
+            
+            if debug:
+                self.debugRequest(r, attempts)
+            
+            # Check for expired session token
+            if r.status_code == 401 and attempts <= self.max_attempts:
+                self.login()
+                attempts = attempts + 1
+                continue
+            # Abort after the maximum number of attempts
+            elif attempts > self.max_attempts:
+                resp = {
+                    'status': r.status_code,
+                    'text': r.text
+                }
+                break
+            # Else if there is an unexpected error return a failure
+            elif r.status_code < 200 or r.status_code > 299:
+                resp = {
+                    'status': r.status_code,
+                    'text': r.text
+                }
+                break
+            elif r.status_code == 204:
+                resp = { 'message': 'User group updated' }
+                break
+            else:
+                resp = r.json()
+                break
+        
+        return resp
+    
+
     #############################
     # Privileges section
     #############################
