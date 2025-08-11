23b6d370b54db8f953d492419ed233006cb42c0a
jonbowring <jbowring@informatica.com>
2025-08-11 20:14:46 +1000

    Improve naming (#25)
    * Updated all option naming
    
    * A big update with lots of clean up and addition of i18n

---
+++ src/idmc_cli/i18n.py
@@ -0,0 +1,30 @@
+import yaml
+from pathlib import Path
+
+I18N_DIR = Path('config')
+I18N_FILE = I18N_DIR / 'i18n.yaml'
+
+class I18n:
+    def __init__(self):
+        self.i18n_path = I18N_FILE
+        self.data = {}
+        self.load()
+
+    def load(self):
+        if self.i18n_path.exists():
+            with open(self.i18n_path, 'r') as f:
+                self.data = yaml.load(f, Loader=yaml.SafeLoader)
+
+    def get(self):
+        return self.data
+    
+    def getHelpOption(self, command, sub_command, option):
+        if sub_command:
+            option = self.data['english']['help'][command][sub_command]['options'][option]
+        else:
+            option = self.data['english']['help'][command]['options'][option]
+        
+        return option
+
+# Expose the class as a variable
+i18n = I18n()
